68fa9291c340a672c8a48b71c2990776386e7e81
Brian Perrett <bperrett@fusionbox.com>
2022-11-30 21:51:11 -0800

    day 1 rust

---
+++ rust/src/bin/day1.rs
@@ -0,0 +1,21 @@
+use::std::fs;
+
+type N = u32;
+type VecN = Vec<N>;
+
+fn main() {
+    let data = fs::read_to_string("../data/day1.txt").unwrap();
+    let mut ndata: VecN = data
+        .trim()
+        .split("\n\n")
+        .into_iter()
+        .map(|x| -> N {
+            let nums: VecN = x.split("\n").map(|x| x.parse().unwrap()).collect();
+            nums.iter().sum()
+        })
+        .collect();
+    ndata.sort();
+    ndata.reverse();
+    println!("{:?}", ndata[0]);
+    println!("{:?}", &ndata[..3].iter().sum::<N>());
+}
