940734b65d522083dd27bbba8f2f6ebf88ff2baf
Brian Perrett <bperrett@fusionbox.com>
2022-12-01 16:59:24 -0800

    I moved files so this broke.

--- python/day01.py
+++ python/day01.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-file = Path(__file__).parent.parent.parent / 'data' / 'day1.txt'
+file = Path(__file__).parent.parent / 'data' / 'day1.txt'
 x = sorted(
     [sum(map(int, x.split())) for x in file.read_text().split('\n\n')],
     reverse=True
