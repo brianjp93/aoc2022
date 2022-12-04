f71794ebc45bc99a3e688d3a819d6d78b8eb014d
Brian Perrett <bperrett@fusionbox.com>
2022-12-04 11:33:50 -0800

    Formatting.

--- rust/src/bin/day4.rs
+++ rust/src/bin/day4.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashSet, fs, cmp};
 use regex::Regex;
+use std::{cmp, collections::HashSet, fs};
 
 fn main() {
     let data = fs::read_to_string("../data/day4.txt").unwrap();
@@ -13,8 +13,8 @@ fn main() {
         let b: usize = cap.get(2).unwrap().as_str().parse().unwrap();
         let c: usize = cap.get(3).unwrap().as_str().parse().unwrap();
         let d: usize = cap.get(4).unwrap().as_str().parse().unwrap();
-        let r1: HashSet<usize> = HashSet::from_iter(a..b+1);
-        let r2: HashSet<usize> = HashSet::from_iter(c..d+1);
+        let r1: HashSet<usize> = HashSet::from_iter(a..b + 1);
+        let r2: HashSet<usize> = HashSet::from_iter(c..d + 1);
 
         if r1.union(&r2).count() == cmp::max(r1.len(), r2.len()) {
             count1 += 1;
