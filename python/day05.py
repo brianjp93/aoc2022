5f7be32cf58762bfe79dfcbc074380d2f7bac2aa
Brian Perrett <perrettbrian@gmail.com>
2022-12-05 22:39:55 -0700

    Reverse but shorter, again

--- python/day05.py
+++ python/day05.py
@@ -15,7 +15,7 @@ def move(crates: list[list[str]], group=False):
     for data in re.findall(r"move (\d+) from (\d+) to (\d+)", arrangement):
         count, start, end = map(int, data)
         start, end = start-1, end-1
-        piece = crates[start][-count:] if group else reversed(crates[start][-count:])
+        piece = crates[start][-count:] if group else crates[start][-count:][::-1]
         crates[end].extend(piece)
         del crates[start][-count:]
     return "".join(x[-1] for x in crates)
