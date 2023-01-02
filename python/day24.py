04199a1d9026b53af0fb1d844948ac3504635a07
Brian Perrett <bperrett@fusionbox.com>
2023-01-01 18:24:28 -0800

    day24

--- python/day24.py
+++ python/day24.py
@@ -1,37 +1,22 @@
-from collections import defaultdict
-from functools import cache
-from itertools import count, product
+from functools import cached_property
 from pathlib import Path
 from dataclasses import dataclass, field
-from typing import Literal, Self, Type
+from typing import Literal, Self
 from queue import PriorityQueue
+from math import lcm
 
 
 file = Path(__file__).parent.parent / "data" / "day24.txt"
 raw = file.read_text()
 
+OPEN = "."
+WALL = "#"
 
-#raw = """
-##.######
-##>>.<^<#
-##.<..<<#
-##>v.><>#
-##<^v^^>#
-#######.#""".strip()
-
-OPEN = '.'
-WALL = '#'
-
-BLIZZARDS: dict[Literal['^', '>', '<', 'v'], tuple[int, int]] = {
-    'v': (0, 1),
-    '>': (1, 0),
-    '^': (0, -1),
-    '<': (-1, 0),
-}
-BLIZZARD_KEYS = set(BLIZZARDS.keys())
-
-rbliz = {
-    val: key for key, val in BLIZZARDS.items()
+BLIZZARDS: dict[Literal["^", ">", "<", "v"], tuple[int, int]] = {
+    "v": (0, 1),
+    ">": (1, 0),
+    "^": (0, -1),
+    "<": (-1, 0),
 }
 
 
@@ -48,16 +33,12 @@ class Point:
         x, y = other if isinstance(other, tuple) else (other.x, other.y)
         return Point(x=self.x - x, y=self.y - y)
 
-    def as_tuple(self):
-        return (self.x, self.y)
-
 
 @dataclass(order=True)
 class Item:
+    heuristic: int
     t: int
-    dist: int
     pos: Point = field(compare=False)
-    moves: list[Point] = field(compare=False)
 
 
 @dataclass(frozen=True)
@@ -67,6 +48,7 @@ class Basin:
     end: Point
     x_length: int
     y_length: int
+    map_cache: dict[int, dict[Point, str]] = field(default_factory=dict)
 
     @classmethod
     def parse(cls, data: str):
@@ -76,64 +58,73 @@ class Basin:
         lines = data.splitlines()
         x_length = len(lines[0]) - 2
         y_length = len(lines) - 2
-        print(f'{x_length=}')
-        print(f'{y_length=}')
         for y, row in enumerate(lines):
             y = y - 1
             for x, ch in enumerate(row):
                 x = x - 1
-                if ch in '.^v<>':
+                if ch in ".^v<>":
                     m[Point(x, y)] = ch
                     if y == -1:
-                        if ch == '.':
+                        if ch == ".":
                             start = Point(x, y)
                     elif y == y_length:
-                        if ch == '.':
+                        if ch == ".":
                             end = Point(x, y)
         assert start and end
         return cls(map=m, start=start, x_length=x_length, y_length=y_length, end=end)
 
-    def dist(self, pos: Point):
-        dx = abs(pos.x - self.end.x)
-        dy = abs(pos.y - self.end.y)
-        return dx + dy
+    @cached_property
+    def tmod(self):
+        return lcm(self.x_length, self.y_length)
 
-    def traverse(self):
+    def dist(self, pos: Point):
+        return abs(pos.x - self.end.x) + abs(pos.y - self.end.y)
+
+    def back_and_forth(self):
+        t1 = self.traverse(self.start, self.end)
+        t2 = self.traverse(self.end, self.start, init_t=t1)
+        t3 = self.traverse(self.start, self.end, init_t=t2)
+        return t3
+
+    def traverse(self, start: Point, end: Point, init_t=0):
         queue: PriorityQueue[Item] = PriorityQueue()
-        queue.put(Item(t=0, dist=self.dist(self.start), pos=self.start, moves=[self.start]))
-        min_t = float('inf')
+        queue.put(Item(heuristic=self.dist(start), t=init_t, pos=start))
+        min_t = float("inf")
         out = None
+        visited: dict[tuple[Point, int], int] = {}
         while not queue.empty():
             item = queue.get()
-            t, dist, pos, moves = item.t,  item.dist, item.pos, item.moves
+            heur, t, pos = item.heuristic, item.t, item.pos
+            key = (item.pos, t % self.tmod)
+            if other_t := visited.get(key, None):
+                if t >= other_t:
+                    continue
+            else:
+                visited[key] = t
+
             m = self.calculate_map(t)
-            if m[pos] in BLIZZARD_KEYS:
+            if m[pos] in BLIZZARDS.keys():
                 continue
 
-            if t + dist >= min_t:
+            if heur >= min_t:
                 continue
 
-            if pos == self.end:
-                # print(moves)
+            if pos == end:
                 if t < min_t:
-                    print(f'Found path with time {t}')
-                    # print(moves)
-                    # self.draw_path(moves)
                     min_t = t
-                    out = t, moves
+                    out = t
                 continue
 
             nt = t + 1
-            queue.put(Item(t=nt, dist=dist, pos=pos, moves=moves[:] + [pos]))
+            queue.put(Item(heuristic=heur + 1, t=nt, pos=pos))
             for adj in BLIZZARDS.values():
                 npos = pos + adj
-                if self.map.get(npos, WALL) != WALL:
-                    queue.put(Item(t=nt, dist=self.dist(npos), pos=npos, moves=moves[:] + [npos]))
+                if npos in self.map:
+                    queue.put(Item(heuristic=nt + self.dist(npos), t=nt, pos=npos))
         assert out
         return out
 
     def draw(self, pos: Point, t: int):
-        print(f'{t=}')
         m = self.calculate_map(t)
         rows = []
         for y in range(-1, self.y_length + 1):
@@ -141,27 +132,29 @@ class Basin:
             for x in range(-1, self.x_length + 1):
                 point = Point(x, y)
                 if pos == point:
-                    row.append('O')
+                    row.append("O")
                 elif point == self.end:
-                    row.append('E')
+                    row.append("E")
                 elif point == self.start:
-                    row.append('S')
+                    row.append("S")
                 else:
                     row.append(m.get(point, WALL))
-            rows.append(''.join(row))
-        print('\n'.join(rows))
+            rows.append("".join(row))
+        print("\n".join(rows))
 
     def draw_path(self, moves: list[Point]):
         for t, pos in enumerate(moves):
             self.draw(pos, t)
             input()
 
-    @cache
     def calculate_map(self, t: int):
+        t = t % self.tmod
+        if ret := self.map_cache.get(t, None):
+            return ret
         m: dict[Point, str] = {}
         for point, val in self.map.items():
-            if val in '<>v^.':
-                m[point] = '.'
+            if val in "<>v^.":
+                m[point] = "."
         for point, val in self.map.items():
             if move := BLIZZARDS.get(val, None):  # type: ignore
                 idx = (move.index(0) + 1) % 2
@@ -174,18 +167,22 @@ class Basin:
                     x = point.x
                     y = ((move[idx] * t) + y0) % self.y_length
                 npoint = Point(x, y)
-                # print(f'{val=}, start={point}, {t=}, calculated: {npoint}')
                 m[npoint] = val
+        self.map_cache[t] = m
         return m
 
 
-basin = Basin.parse(raw)
-# print(basin)
-t, moves = basin.traverse()
-print(t)
+def part1():
+    basin = Basin.parse(raw)
+    t = basin.traverse(basin.start, basin.end)
+    return t
 
 
-# basin = Basin.parse(raw)
-# for t in range(20):
-#     basin.draw(Point(-1, -1), t)
-#     input()
+def part2():
+    basin = Basin.parse(raw)
+    t = basin.back_and_forth()
+    return t
+
+
+print(f'{part1()=}')
+print(f"{part2()=}")
