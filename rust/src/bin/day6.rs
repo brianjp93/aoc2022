7f174763be11c7e7ee27f0624f625ba24f149aa0
Brian Perrett <perrettbrian@gmail.com>
2023-05-20 01:18:44 -0600

    Make it faster.

--- rust/src/bin/day6.rs
+++ rust/src/bin/day6.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{collections::HashMap, fs};
 
 fn main() {
     let data = fs::read_to_string("../data/day6.txt").unwrap();
@@ -12,10 +12,34 @@ fn main() {
 }
 
 fn find_start(data: &str, count: usize) -> usize {
-    for i in 0..data.len() {
-        let letters: HashSet<char> = data[i..i+count].chars().collect();
+    let mut letters: HashMap<char, usize> = HashMap::new();
+    let data_chars: Vec<char> = data.chars().collect();
+    for i in 0..count {
+        let ch = data_chars[i];
+        if let Some(val) = letters.get_mut(&ch) {
+            *val += 1;
+        } else {
+            letters.insert(ch.clone(), 1);
+        }
+    }
+    for i in count..data_chars.len() {
         if letters.len() == count {
-            return i + count
+            return i
+        }
+        let ch = data_chars[i-count];
+        if let Some(val) = letters.get_mut(&ch) {
+            if val == &mut 1 {
+                letters.remove(&ch);
+            }
+            else {
+                *val -= 1;
+            }
+        }
+        let ch = data_chars[i];
+        if let Some(val) = letters.get_mut(&ch) {
+            *val += 1;
+        } else {
+            letters.insert(ch.clone(), 1);
         }
     }
     panic!("Expected to find value before ending loop.");
